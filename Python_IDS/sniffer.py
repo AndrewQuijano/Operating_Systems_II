201d4b7999475d652b2c1d25584e2eba641237c2
Daniel Mesko <dpmesko@gmail.com>
2019-02-22 18:20:38 -0500

    [WIP] Configuring sniffer interface, moving to VM

--- Python_IDS/sniffer.py
+++ Python_IDS/sniffer.py
@@ -11,10 +11,11 @@
 
 import pyshark
 
-capture = pyshark.LiveCapture(interface='eth0')
+# TODO: Have to specify interface...should we take interface as input from a driver module?
+capture = pyshark.LiveCapture(interface='en3')
 
-for packet in capture.sniff_continuously(packet_count=10):
+
+for packet in capture.sniff(packet_count=10):
 
 	print("highest layer: {}".format(packet.highest_layer))
-
 
