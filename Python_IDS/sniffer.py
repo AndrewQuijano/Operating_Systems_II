60e2babb2ce9976503d20e98805e3416af32be4d
Daniel Mesko <dpmesko@gmail.com>
2019-02-22 18:50:13 -0500

    [WIP] Experimenting with packet sniffing, logging

--- Python_IDS/sniffer.py
+++ Python_IDS/sniffer.py
@@ -2,8 +2,7 @@
 
 # --------------------------------------------------------
 #   Network Packet Sniffer:
-#     parses packets into a struct containing all header
-#       fields
+#     parses packets 
 #
 #    Authors: Daniel Mesko and Andrew Quijano 
 # --------------------------------------------------------
@@ -12,10 +11,10 @@
 import pyshark
 
 # TODO: Have to specify interface...should we take interface as input from a driver module?
-capture = pyshark.LiveCapture(interface='en3')
+capture = pyshark.LiveCapture(interface='enp0s3')
 
+for packet in capture.sniff_continuously(packet_count=5):
 
-for packet in capture.sniff(packet_count=10):
+    packet.pretty_print()
 
-	print("highest layer: {}".format(packet.highest_layer))
 
