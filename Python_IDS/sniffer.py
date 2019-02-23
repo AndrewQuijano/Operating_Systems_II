35f7189e28f51832d1737f73f941d83e539056a9
Daniel Mesko <dpmesko@gmail.com>
2019-02-22 23:27:16 -0500

    packet loop in place , still need to figure out feature extraction and interface with ML thread

--- Python_IDS/sniffer.py
+++ Python_IDS/sniffer.py
@@ -13,8 +13,18 @@ import pyshark
 # TODO: Have to specify interface...should we take interface as input from a driver module?
 capture = pyshark.LiveCapture(interface='enp0s3')
 
-for packet in capture.sniff_continuously(packet_count=5):
-
-    packet.pretty_print()
-
-
+#TODO: Figure out interface with ML stuff...
+#         Extract features, pass back as object to a separate thread for ML calcs ?
+def handle_packet(pkt):
+    try:
+        print("highest layer" + str(pkt.highest_layer))
+        protocol =  pkt.transport_layer
+        src_addr = pkt.ip.src
+        src_port = pkt[pkt.transport_layer].srcport
+        dst_addr = pkt.ip.dst
+        dst_port = pkt[pkt.transport_layer].dstport
+        print ('%s  %s:%s --> %s:%s  %s ' % (protocol, src_addr, src_port, dst_addr, dst_port, pkt.de))
+    except AttributeError as e:
+        pass
+ 
+capture.apply_on_packets(handle_packet, timeout=100)
