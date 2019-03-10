18f00d8bb43f89f03fe5a45a05d3a49c09f30777
AndrewQuijano <AndrewQuijano92@gmail.com>
2019-03-10 19:46:05 -0400

    made few changes, got other things to finish first

--- Python_IDS/Incremental_ML/incremental_bayes.py
+++ Python_IDS/Incremental_ML/incremental_bayes.py
@@ -1,8 +1,7 @@
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import brier_score_loss
 
 
-def build_bayes(x_train, y_train, x_test=None, y_test=None):
+def build_bayes(x_train, y_train):
     # Gaussian Naive-Bayes with no calibration
     clf = MultinomialNB(class_prior=None, fit_prior=True)
     clf.partial_fit(x_train, y_train)
