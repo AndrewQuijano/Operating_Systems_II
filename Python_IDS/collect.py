6f82b425608d4ac0053cd8e579b9a727b56988c5
Daniel Mesko <dpmesko@gmail.com>
2019-03-10 23:11:50 -0400

    more packet processing, connection records

--- Python_IDS/collect.py
+++ Python_IDS/collect.py
@@ -8,41 +8,83 @@
 # --------------------------------------------------------
 
 
-
-# TODO: Have to specify interface...should we take interface as input from a driver module?
-
 import pyshark
 
+class Packet:
 
-# collect packets into connection records
+    # protocol-specific members set as None in constructor
+    def __init__(self, con_no, src_ip, dst_ip, src_port, dst_port): 
+        self.con_no = con_no
+        self.src_ip = src_ip
+        self.dst_ip = dst_ip
+        self.src_port = src_port
+        self.dst_port = dst_port
+        self.time_elapsed = None
+
+    def to_string(self):
+        out_str = ('\ntcp_connection_number: ' + str(self.con_no) + 
+            '\nsource IP: ' + str(self.src_ip) +
+            '\ndestination IP: ' + str(self.dst_ip) +
+            '\nsource port: ' + str(self.src_port) +
+            '\ndestination port: ' + str(self.dst_port) +
+            '\ntime elapsed since first packet of connection: ' + 
+            str(self.time_elapsed) + 
+            '\n')
+        return out_str
+
+
+# unnecessary?
+class Connection:
+
+    def __init__(self, duration):
+        self.duration = duration
+
+
+
+# collection function
 def collect_connections(pcap_file):
 
     cap = pyshark.FileCapture(pcap_file)
     data = ''
     connections = {}
 
+    # collect packets into connection records
     for pkt in cap:
         if 'tcp' in pkt:
             key = "tcp_conn"+pkt.tcp.stream
-            data = str(pkt.tcp)
+            pkt_obj = Packet(pkt.tcp.stream, pkt.ip.src, pkt.ip.dst,
+                    pkt.tcp.srcport, pkt.tcp.dstport)
+            pkt_obj.time_elapsed = pkt.tcp.time_relative
         elif 'udp' in pkt:
             key = "udp_conn"+pkt.udp.stream
-            data = str(pkt.udp)
+            pkt_obj = Packet(pkt.udp.stream, pkt.ip.src, pkt.ip.dst,
+                    pkt.udp.srcport, pkt.udp.dstport)
         else:
-            key = "other_transport"
+            # do not record packets that aren't TCP/UDP
+            continue
 
         if key not in connections.keys():
-            connections[key] = data
+            connections[key] = [pkt_obj]
         else:
-            connections[key] += "\n" + data
+            lst = connections[key]
+            lst.append(pkt_obj)
 
+
+
+    # derive features, generate output file
+#    out_file = open('packets.csv', 'w')
     for k,v in connections.items():
         out_file = k + '.bin'
         output = open(out_file, 'w')
-        output.write(v)
-        output.flush()
+        
+        for pkt in v:
+            output.write(pkt.to_string())
+            output.flush()
+
+
+
 
 if __name__ == '__main__':
     pcap_file = 'sniff.pcap'
     collect_connections(pcap_file)
-    print('connection records generated\n')
+    print('\nconnection records generated\n')
--- Python_IDS/start_sniffing.sh
+++ Python_IDS/start_sniffing.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# take number of packets to sniff as arg?
 
-sudo tcpdump -c 20 -s0 -i enp0s3 -w sniff.pcap
+# TODO: Take number of packets/timeout as arg?
+#         take interface name as arg?
+
+
+sudo tcpdump -c 200 -s0 -i enp0s3 -w sniff.pcap
 python3 collect.py
 
 # tshark -r sniff.pcap -T fields -e frame.time -e ip.flags -e ip.src -e ip.dst -e ip.proto -e tcp.connection.fin -e tcp.dstport -e tcp.srcport -E header=y -E separator=, -E quote=d -E occurrence=f > packets.csv
