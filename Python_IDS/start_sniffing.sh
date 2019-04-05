aa136ebdf6aa2b50d2004f34c46c3da8a6a11880
Daniel Mesko <dpmesko@gmail.com>
2019-04-05 00:14:20 -0400

    status echo statements to make it look like a proper script

--- Python_IDS/start_sniffing.sh
+++ Python_IDS/start_sniffing.sh
@@ -4,14 +4,17 @@
 # TODO: Take number of packets/timeout as arg?
 #         take interface name as arg?
 
-
+echo "reading raw packet data from the write"
 sudo tcpdump -c 200 -s0 -i enp0s3 -w sniff.pcap
 python3 collect.py
+echo "feeding connection records into ML module"
+
+
 
 # tshark -r sniff.pcap -T fields -e frame.time -e ip.flags -e ip.src -e ip.dst -e ip.proto -e tcp.connection.fin -e tcp.dstport -e tcp.srcport -E header=y -E separator=, -E quote=d -E occurrence=f > packets.csv
 
 
 # clean up
 sudo rm sniff.pcap
-
+#sudo rm packets.csv
 
