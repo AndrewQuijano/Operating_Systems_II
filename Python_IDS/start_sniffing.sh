712cc6527bd432aa1c81ca68e04fbe513957c2d3
Daniel Mesko <dpmesko@gmail.com>
2019-04-10 15:37:58 -0400

    fixed typo

--- Python_IDS/start_sniffing.sh
+++ Python_IDS/start_sniffing.sh
@@ -4,7 +4,7 @@
 # TODO: Take number of packets/timeout as arg?
 #         take interface name as arg?
 
-echo "reading raw packet data from the write"
+echo "reading raw packet data from the wire"
 sudo tcpdump -c 200 -s0 -i enp0s3 -w sniff.pcap
 python3 collect.py
 echo "feeding connection records into ML module"
