78de5956c876f08db502b571982efe30b838cffb
Daniel Mesko <dpmesko@gmail.com>
2019-04-12 03:18:57 -0400

    bigger tcpdump packet count, removed crap

--- Python_IDS/Sniffer/start_sniffing.sh
+++ Python_IDS/Sniffer/start_sniffing.sh
@@ -5,13 +5,9 @@
 #         take interface name as arg?
 
 echo "reading raw packet data from the wire"
-sudo tcpdump -c 200 -s0 -i enp0s3 -w sniff.pcap
+sudo tcpdump -c 500 -s0 -i enp0s3 -w sniff.pcap
 python3 collect.py
-echo "feeding connection records into ML module"
-
-
-
-# tshark -r sniff.pcap -T fields -e frame.time -e ip.flags -e ip.src -e ip.dst -e ip.proto -e tcp.connection.fin -e tcp.dstport -e tcp.srcport -E header=y -E separator=, -E quote=d -E occurrence=f > packets.csv
+#echo "feeding connection records into ML module"
 
 
 # clean up
