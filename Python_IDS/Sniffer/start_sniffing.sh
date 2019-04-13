4ac829d1ec328c4e813433efea757c3cb1e22551
Daniel Mesko <dpmesko@gmail.com>
2019-04-13 17:00:34 -0400

    reduced sniff size

--- Python_IDS/Sniffer/start_sniffing.sh
+++ Python_IDS/Sniffer/start_sniffing.sh
@@ -5,7 +5,7 @@
 #         take interface name as arg?
 
 echo "reading raw packet data from the wire"
-sudo tcpdump -c 500 -s0 -i enp0s3 -w sniff.pcap
+sudo tcpdump -c 200 -s0 -i enp0s3 -w sniff.pcap
 python3 collect.py
 #echo "feeding connection records into ML module"
 
