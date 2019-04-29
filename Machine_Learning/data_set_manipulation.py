876b840c1b21c7bd2daa6326cbfbe70e2367c44c
AndrewQuijano <andrewquijano92@gmail.com>
2019-04-29 04:42:43 -0400

    Need to redo kdd quickly

--- Machine_Learning/data_set_manipulation.py
+++ Machine_Learning/data_set_manipulation.py
@@ -1,30 +1,37 @@
-import csv
+import pandas as pd
 
 
 # For kdd, we skip columns 10 - 22
 # In our version, the label is in 0, so don't drop that!
 def drop_columns(file, begin=0, end=9, begin_2=23, end_2=42):
-    with open(file, "r") as source:
-        rdr = csv.reader(source)
-        with open("new_" + file, "w") as result:
-            wtr = csv.writer(result)
-            for r in rdr:
-                wtr.writerow(r[begin:end] + r[begin_2:end_2])
+    # with open(file, "r") as source:
+    #    rdr = csv.reader(source)
+    #    with open("new_" + file, "w") as result:
+    #        wtr = csv.writer(result)
+    #        for r in rdr:
+    #            wtr.writerow(r[begin:end] + r[begin_2:end_2])
 
+    range1 = [i for i in range(begin, end)]
+    range2 = [i for i in range(begin_2, end_2)]
+    usecols = range1 + range2
+    df = pd.read_csv(file, usecols=usecols)
+    df.to_csv("modified_" + file)
 
+# Remember Github's Limit is 100 MB or 100,000 KB
 def split_csv(file, size=1000000):
-    file_part = 1
+    file_part = 0
+    idx = 1
     lines = []
     with open(file, 'r') as big_file:
         for line in big_file:
             if file_part % size == 0:
-                with open(str(file_part) + "_" + str(file), 'w+') as chunk:
+                with open(str(idx) + "_" + str(file), 'w+') as chunk:
                     chunk.writelines(lines)
                 lines = []
+                idx += 1
             else:
                 lines.append(line)
             file_part += 1
-            file_part = file_part % size
 
 
 def merge_csv(file):
@@ -38,7 +45,5 @@ def merge_csv(file):
 
 
 if __name__ == "__main__":
-    # main()
-    # kdd_prep()
-    # drop_columns("kdd_prep.csv")
+    drop_columns("kdd_prep.csv")
     split_csv("kdd_prep.csv")
