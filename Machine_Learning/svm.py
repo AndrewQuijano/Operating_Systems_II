d3e530e84273f7ad21dacc82fb10ed5028309f41
AndrewQuijano <andrewquijano92@gmail.com>
2019-05-03 17:40:16 -0400

    Considering how fast 2 fold validation came, increase to 5 and get ALL classifers down NOW

--- Machine_Learning/svm.py
+++ Machine_Learning/svm.py
@@ -7,7 +7,7 @@ import time
 
 
 def svc_rbf_param_selection(x, y, n_folds=10, slow=True):
-    c = np.arange(0.1, 1, 0.1)
+    c = np.arange(0.01, 1, 0.01)
     gammas = np.arange(0.1, 1, 0.1)
     random_grid = {
         'C': c,
@@ -26,7 +26,7 @@ def svc_rbf_param_selection(x, y, n_folds=10, slow=True):
 
 
 def svc_linear_param_selection(x, y, n_folds=10, slow=False):
-    c = np.arange(0.1, 1, 0.1)
+    c = np.arange(0.01, 1, 0.01)
     param_grid = {'C': c}
     model = svm.SVC(kernel='linear')
     if slow:
