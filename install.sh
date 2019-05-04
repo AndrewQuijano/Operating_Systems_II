3fb398fd3eab4e8dba0e93ba2a2368d6d392c652
AndrewQuijano <AndrewQuijano92@gmail.com>
2019-05-04 14:11:31 -0700

    begin testing NSL on VM at home

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install git
+
+# Install basics
+sudo apt-get install vim
+sudo apt-get install dos2unix
+
+# Install Python and packages
+sudo apt-get install python3-pip
+sudo apt-get install tcpdump python3-crypto ipython3
+sudo apt-get install python3-tk
+
+# Install Python and packages used by IDS and Fuzzer
+sudo pip3 install scapy-python3
+sudo pip3 install sklearn
+sudo pip3 install matplotlib
+sudo pip3 install pandas
+sudo pip3 install pyshark
+sudo pip3 install joblib
+sudo pip3 install pikepdf
+
+# Update the packages, Written as of May 4, 2019
+# You want sklearn 0.20.3 scipy 1.1.0 numpy 1.16.3
+sudo pip3 install scipy -U
+sudo pip3 install sklearn -U
+sudo pip3 install numpy -U
