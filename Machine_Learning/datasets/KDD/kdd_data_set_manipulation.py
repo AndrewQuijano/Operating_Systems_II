06ad1179ae419dac3e7f815cc79339f35f50da29
AndrewQuijano <andrewquijano92@gmail.com>
2019-05-08 03:10:06 -0400

    prepared tools to test rebuilding kdd

--- Machine_Learning/datasets/KDD/kdd_data_set_manipulation.py
+++ Machine_Learning/datasets/KDD/kdd_data_set_manipulation.py
@@ -1,8 +1,10 @@
+#!/usr/bin/python3
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sys import argv
 from os.path import basename
 from subprocess import call
+from os import geteuid
 
 
 # For kdd, we skip columns 10 - 22
@@ -32,9 +34,9 @@ def split_csv(file, size=500000):
             file_part += 1
 
 
-def merge_csv(file):
+def merge_csv(file, n_parts=9):
     file_part = 1
-    for j in range(9):
+    for j in range(n_parts):
         with open(str(file_part) + "_" + file + ".csv", 'r') as chunk:
             with open(file + ".csv", 'a+') as big_file:
                 for line in chunk:
@@ -67,20 +69,20 @@ def kdd_prep(file):
 
     # Have list of stuff
     y = ["normal.", "back.", "buffer_overflow.", "ftp_write.", "guess_passwd.",
-                 "imap.", "ipsweep.", "land.", "loadmodule.", "multihop.", "neptune.",
-                 "nmap.", "perl.", "phf.", "pod.", "portsweep.", "rootkit.", "satan.", "smurf.",
-                 "spy.", "teardrop.", "warezclient.", "warezmaster."]
+         "imap.", "ipsweep.", "land.", "loadmodule.", "multihop.", "neptune.",
+         "nmap.", "perl.", "phf.", "pod.", "portsweep.", "rootkit.", "satan.", "smurf.",
+         "spy.", "teardrop.", "warezclient.", "warezmaster."]
     proto = ["tcp", "udp", "icmp"]
     fl = ["SF", "S2", "S1", "S3", "OTH", "REJ", "RSTO", "S0", "RSTR", "RSTOS0", "SH"]
     serv = ["http", "smtp", "domain_u", "auth", "finger", "telnet", "eco_i", "ftp", "ntp_u",
-               "ecr_i", "other", "urp_i", "private", "pop_3", "ftp_data", "netstat", "daytime", "ssh",
-               "echo", "time", "name", "whois", "domain", "mtp", "gopher", "remote_job", "rje", "ctf",
-               "supdup", "link", "systat", "discard", "X11", "shell", "login", "imap4", "nntp", "uucp",
-               "pm_dump", "IRC", "Z39_50", "netbios_dgm", "ldap", "sunrpc", "courier", "exec", "bgp",
-               "csnet_ns", "http_443", "klogin", "printer", "netbios_ssn", "pop_2", "nnsp", "efs",
-               "hostnames", "uucp_path", "sql_net", "vmnet", "iso_tsap", "netbios_ns", "kshell",
-               "urh_i", "http_2784", "harvest", "aol",
-                "tftp_u", "http_8001", "tim_i", "red_i"]
+            "ecr_i", "other", "urp_i", "private", "pop_3", "ftp_data", "netstat", "daytime", "ssh",
+            "echo", "time", "name", "whois", "domain", "mtp", "gopher", "remote_job", "rje", "ctf",
+            "supdup", "link", "systat", "discard", "X11", "shell", "login", "imap4", "nntp", "uucp",
+            "pm_dump", "IRC", "Z39_50", "netbios_dgm", "ldap", "sunrpc", "courier", "exec", "bgp",
+            "csnet_ns", "http_443", "klogin", "printer", "netbios_ssn", "pop_2", "nnsp", "efs",
+            "hostnames", "uucp_path", "sql_net", "vmnet", "iso_tsap", "netbios_ns", "kshell",
+            "urh_i", "http_2784", "harvest", "aol",
+            "tftp_u", "http_8001", "tim_i", "red_i"]
 
     # Fit them with the known classes
     classes.fit(y)
@@ -180,6 +182,15 @@ def n_col(file_name):
                 break
 
 
+def n_row(file_name):
+    size = 0
+    with open(file_name) as data:
+        for line in data:
+            if line is not None:
+                size += 1
+    return size
+
+
 def remove_rows(file_name, remove_labels):
     with open(file_name) as read_kdd_data, open("./kddcup_filtered.csv", "w") as write_kdd:
         for line in read_kdd_data:
@@ -208,21 +219,19 @@ def process(file_path, new_file=None):
     file_name = basename(file_path)
     file_parts = file_name.split('.')
     if new_file is None:
-        with open(file_parts[0] + ".csv", "w") as f:
+        with open(str(file_parts[0]) + ".csv", "w") as f:
             call(["sudo", "../kdd99extractor", file_path], stdout=f)
     else:
         with open(new_file, "w") as f:
             call(["sudo", "../kdd99extractor", file_path], stdout=f)
 
 
-# To convert KDD
-# 1- First Swap columns
-# 2- Encode it
-# 2- Drop Columns that are content related
-# 3- Split into parts
-# **To use it, just merge it, use raw file name w/o extension!**
-if __name__ == "__main__":
-    with open("batch.txt") as f:
+def build_kdd():
+    if geteuid() != 0:
+        print("Need root permission!")
+        exit(0)
+
+    with open("./build_kdd.txt", "r") as f:
         for line in f:
             args = line.split()
             if args[0] == "pcap" and len(args) == 3:
@@ -231,8 +240,52 @@ if __name__ == "__main__":
                 process(args[1], args[2])
     print("Complete conversion to PCAP!")
 
-    for i in range(34):
+    for i in range(35):
         process("kdd_" + str(i) + ".pcap")
+    merge_csv("kdd", n_parts=35)
 
-    # Merge into KDD
-    merge_csv("kdd")
+
+# 1- Filter out duplicates
+# 2- Any service with ICMP? KIll it
+def nsl_kdd_filter(kdd_file):
+    filter_row = {}
+    with open(kdd_file) as read_kdd_data:
+        for line in read_kdd_data:
+            # Swap using encoder
+            line = line.rstrip()
+            # As my ML stuff excepts class on first column
+            if line in filter_row:
+                filter_row[line] = 1
+            else:
+                filter_row[line] = filter_row[line] + 1
+    # Print frequency data?
+    rows = list(filter_row.keys())
+    with open("./NSL_KDD.csv", "w") as write_kdd:
+        for r in rows:
+            write_kdd.write(r + '\n')
+
+
+def compare_csv(kdd_file, nsl_kdd):
+    size = 0
+    similarity = 0
+    with open(kdd_file, "r") as read_kdd_data, open(nsl_kdd, "r") as read_nsl_data:
+        for kdd_line, nsl_line in read_kdd_data, read_nsl_data:
+            if kdd_line == nsl_line:
+                similarity = similarity + 1
+            size = size + 1
+
+
+# To convert KDD
+# 1- First Swap columns AND Encode it
+# 2- Drop Columns that are content related
+# 3- Split into parts
+# **To use it, just merge it, use raw file name w/o extension!**
+if __name__ == "__main__":
+    # kdd_prep("kddcup.csv")
+    # rop_columns("kddcup_prep.csv")
+    # split_csv("modified_kddcup_prep.csv")
+    build_kdd()
+    nsl_kdd_filter("kdd.csv")
+    n_row("kdd.csv")
+    n_row("NSL_KDD.csv")
+    # compare_csv("kdd.csv", "NSL_KDD.csv")
