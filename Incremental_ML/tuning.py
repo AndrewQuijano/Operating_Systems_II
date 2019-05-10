34ae1e1617e04146f5b1c2f81df9bd8635923085
AndrewQuijano <andrewquijano92@gmail.com>
2019-05-10 07:18:20 -0400

    printing to know which classifiers

--- Incremental_ML/tuning.py
+++ Incremental_ML/tuning.py
@@ -7,6 +7,7 @@ import numpy as np
 
 
 def tune_bayes(x, y, n_folds=10, slow=True):
+    print("Tuning Multinomial Bayes...")
     c = np.arange(0.01, 1.3, 0.01)
     param_grid = {'alpha': c}
     model = MultinomialNB()
@@ -17,10 +18,12 @@ def tune_bayes(x, y, n_folds=10, slow=True):
     true_model.fit(x, y)
     if slow:
         plot_grid_search(true_model.cv_results_, c, 'Multinomial_Bayes')
+    print("Finished Tuning Multinomial Bayes...")
     return true_model
 
 
 def tune_perceptron(x, y, n_folds=10, slow=True):
+    print("Tuning Perceptron...")
     c = np.arange(0.01, 1.3, 0.01)
     param_grid = {'alpha': c}
     model = Perceptron(tol=1e-3, warm_start=True)
@@ -31,10 +34,12 @@ def tune_perceptron(x, y, n_folds=10, slow=True):
     true_model.fit(x, y)
     if slow:
         plot_grid_search(true_model.cv_results_, c, "Perceptron")
+    print("Finished Tuning Perceptron...")
     return true_model
 
 
 def tune_sgd_clf(x, y, n_folds=10, slow=True):
+    print("Tuning SGD Classifier...")
     c = np.arange(0.0001, 0.01, 0.00001)
     param_grid = {'alpha': c}
     model = SGDClassifier(warm_start=True, tol=1e-3)
@@ -45,10 +50,12 @@ def tune_sgd_clf(x, y, n_folds=10, slow=True):
     true_model.fit(x, y)
     if slow:
         plot_grid_search(true_model.cv_results_, c, 'SGD_Classifier')
+    print("Finished Tuning SGD Classifier...")
     return true_model
 
 
 def tune_sgd_reg(x, y, n_folds=10, slow=True):
+    print("Tuning SGD Regressor...")
     c = np.arange(0.0001, 0.01, 0.00001)
     param_grid = {'alpha': c}
     model = SGDRegressor(warm_start=True, tol=1e-3)
@@ -59,10 +66,12 @@ def tune_sgd_reg(x, y, n_folds=10, slow=True):
     true_model.fit(x, y)
     if slow:
         plot_grid_search(true_model.cv_results_, c, 'SGD_Regression')
+    print("Finished Tuning SGD Regressor...")
     return true_model
 
 
 def tune_passive_aggressive_clf(x, y, n_folds=10, slow=True):
+    print("Tuning Passive Aggressive Classifier...")
     c = np.arange(0.01, 1.6, 0.01)
     param_grid = {'C': c}
     model = PassiveAggressiveClassifier(warm_start=True, tol=1e-3)
@@ -73,10 +82,12 @@ def tune_passive_aggressive_clf(x, y, n_folds=10, slow=True):
     true_model.fit(x, y)
     if slow:
         plot_grid_search(true_model.cv_results_, c, 'Passive_Aggressive_CLF')
+    print("Finished Tuning Passive Aggressive Classifier...")
     return true_model
 
 
 def tune_passive_aggressive_reg(x, y, n_folds=10, slow=True):
+    print("Tuning Passive Regression Classifier...")
     c = np.arange(0.01, 1.6, 0.01)
     param_grid = {'C': c}
     model = PassiveAggressiveRegressor(warm_start=True, tol=1e-3)
@@ -87,4 +98,5 @@ def tune_passive_aggressive_reg(x, y, n_folds=10, slow=True):
     true_model.fit(x, y)
     if slow:
         plot_grid_search(true_model.cv_results_, c, 'Passive_Aggressive_Regression')
+    print("Finished Tuning Passive Aggressive Regression...")
     return true_model
